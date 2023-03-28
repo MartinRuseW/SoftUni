d3d94adaac4f823c6bc2954cd5f75aeb76504d60
MartinRuseW <114416024+MartinRuseW@users.noreply.github.com>
2023-03-29 00:27:33 +0300

    Delete Programming Basics - SoftUni/Вложени цикли directory

--- Programming Basics - SoftUni/Вложени цикли/Lab/multiplicationTable.js
+++
@@ -1,9 +0,0 @@
-function multiplicationTable(input) {
-
-    for(let i = 1; i <= 10; i++) {
-        for(let k = 1; k <= 10; k++) {
-            console.log(`${i} * ${k} = ${i * k}`)
-        }
-    }
-}
-multiplicationTable()
