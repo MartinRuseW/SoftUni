7b655043922a2d561020ef6a3057b126f2ab62e3
MartinRuseW <114416024+MartinRuseW@users.noreply.github.com>
2023-03-28 23:52:17 +0300

    Delete Programming Basics - SoftUni/For-цикъл/Lab directory

--- Programming Basics - SoftUni/For-цикъл/Lab/numberFrom0To100.js
+++
@@ -1,7 +0,0 @@
-function numberFrom1To100(input) {
-
-    for(let i = 1; i <= 100; i++) {
-    console.log(i);
-    }
-}
-numberFrom1To100([])
--- Programming Basics - SoftUni/For-цикъл/Lab/numbers1NWith3Steps.js
+++
@@ -1,9 +0,0 @@
-function numbers1NWith3Steps(input) {
-
-    let n = Number(input[0]);
-
-    for (let i = 1; i <= n; i += 3) {
-        console.log(i);
-    }
-}
-numbers1NWith3Steps(["10"])
