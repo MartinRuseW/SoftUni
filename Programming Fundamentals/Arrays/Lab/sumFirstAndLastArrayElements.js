e8f054eb1ebae3acc902a5762386bcada290a74e
MartinRuseW <114416024+MartinRuseW@users.noreply.github.com>
2023-04-01 19:02:04 +0300

    Delete Programming Fundamentals/Arrays directory

--- Programming Fundamentals/Arrays/Lab/sumFirstAndLastArrayElements.js
+++
@@ -1,9 +0,0 @@
-function sumFirstAndLastArrayElements(numbers) {
-
-    let firstArray = numbers[0];
-    let lastArray = numbers[numbers.length - 1];
-    let sum = firstArray + lastArray;
-
-    console.log(sum)
-}
-sumFirstAndLastArrayElements([20, 30, 40])
