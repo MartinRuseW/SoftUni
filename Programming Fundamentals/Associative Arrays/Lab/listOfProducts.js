fd4fe6b625ccd08ab7f86e88c0bc20f3fb7d5e59
MartinRuseW <114416024+MartinRuseW@users.noreply.github.com>
2023-04-01 18:58:29 +0300

    Delete Programming Fundamentals/Associative Arrays/Lab directory

--- Programming Fundamentals/Associative Arrays/Lab/listOfProducts.js
+++
@@ -1,9 +0,0 @@
-function listOfProducts(array) {
-
-    let sortedArray = array.sort();
-
-    for (let i = 0; i < sortedArray.length; i++) {
-        console.log(`${i + 1}.${sortedArray[i]}`);
-    }  
-}
-listOfProducts(['Potatoes', 'Tomatoes', 'Onions', 'Apples'])
