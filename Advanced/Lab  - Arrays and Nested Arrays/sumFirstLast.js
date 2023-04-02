e248f2c7286b883d7fb6ff820b3a615c66221de4
MartinRuseW <114416024+MartinRuseW@users.noreply.github.com>
2023-04-02 17:26:01 +0300

    Delete Advanced/Lab  - Arrays and Nested Arrays directory

--- Advanced/Lab  - Arrays and Nested Arrays/sumFirstLast.js
+++
@@ -1,8 +0,0 @@
-function sumFirstLast(array) {
-
-    let firstNumber = array[0];
-    let lastNumber = array.pop();
-    let result = Number(firstNumber) + Number(lastNumber);
-    console.log(result);
-}
-sumFirstLast(['20', '30', '40'])
